49a1e90f3cea917514669cdb190be460bace2967
Roberto Pantoja <74929633+RobertPantoja@users.noreply.github.com>
2025-07-05 11:35:50 -0600

    Add TrackList component and styles to Playlist and SearchResults

---
+++ client/src/Components/TrackList/TrackList.js
@@ -0,0 +1,11 @@
+import "./TrackList.css";
+
+function TrackList({}) {
+  return (
+    <div className="TrackList">
+      {/* <!--  Map method that renders a set of Tracks  --> */}
+    </div>
+  );
+}
+
+export default TrackList;
