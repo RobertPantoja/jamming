1055567faf2adefcf2c54ed899a50a0d63d9d900
Roberto Pantoja <74929633+RobertPantoja@users.noreply.github.com>
2025-07-06 19:35:10 -0600

    Add Spotify authentication utility functions

--- client/src/Components/App/App.js
+++ client/src/Components/App/App.js
@@ -7,12 +7,12 @@ import Playlist from "../Playlist/Playlist";
 
 import mockSearchResults from "../../mocks/mockSearchResults";
 import mockPlaylistTracks from "../../mocks/mockPlaylistTracks";
+import { redirectToSpotifyAuth } from "../../utils/SpotifyAuth";
 
 function App() {
   const [playlistName, setPlaylistName] = useState("My Playlist Name");
   const [searchResults, setSearchResults] = useState(mockSearchResults);
   const [playlistTracks, setPlaylistTracks] = useState(mockPlaylistTracks);
-  const [trackURIs, setTrackURIs] = useState([]);
 
   function addTrack(track) {
     if (playlistTracks.find((savedTack) => savedTack.id === track.id)) {
---
+++ client/src/utils/SpotifyAuth.js
@@ -0,0 +1,49 @@
+function generateRandomString(length) {
+  const possible =
+    "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+  let text = "";
+  for (let i = 0; i < length; i++) {
+    text += possible.charAt(Math.floor(Math.random() * possible.length));
+  }
+  return text;
+}
+
+function base64urlencode(str) {
+  return btoa(String.fromCharCode(...new Uint8Array(str)))
+    .replace(/\+/g, "-")
+    .replace(/\//g, "_")
+    .replace(/=+$/, "");
+}
+
+async function sha256(plain) {
+  const encoder = new TextEncoder();
+  const data = encoder.encode(plain);
+  const digest = await window.crypto.subtle.digest("SHA-256", data);
+  return digest;
+}
+
+export async function redirectToSpotifyAuth() {
+  const clientId = process.env.REACT_APP_SPOTIFY_CLIENT_ID;
+  const redirectUri = "http://127.0.0.1:3000/callback";
+  const scope = [
+    "playlist-modify-public",
+    "playlist-modify-private",
+    "user-read-private",
+    "user-read-email",
+  ].join(" ");
+
+  const codeVerifier = generateRandomString(128);
+  const codeChallenge = base64urlencode(await sha256(codeVerifier));
+
+  localStorage.setItem("code_verifier", codeVerifier);
+
+  const authUrl = new URL("https://accounts.spotify.com/authorize");
+  authUrl.searchParams.set("response_type", "code");
+  authUrl.searchParams.set("client_id", clientId);
+  authUrl.searchParams.set("scope", scope);
+  authUrl.searchParams.set("redirect_uri", redirectUri);
+  authUrl.searchParams.set("code_challenge_method", "S256");
+  authUrl.searchParams.set("code_challenge", codeChallenge);
+
+  window.location.href = authUrl.toString();
+}
