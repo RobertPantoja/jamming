09d6dcc661d34852a52595b52a22674c5ff22735
Roberto Pantoja <74929633+RobertPantoja@users.noreply.github.com>
2025-07-07 14:59:33 -0600

    Add AppRoutes component for routing and navigation

---
+++ client/src/Components/AppRoutes/AppRoutes.js
@@ -0,0 +1,14 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import SpotifyCallback from "../Callback/SpotifyCallback";
+import App from "../App/App";
+
+function AppRoutes() {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<App />} />
+        <Route path="callback" element={<SpotifyCallback />} />
+      </Routes>
+    </Router>
+  );
+}
